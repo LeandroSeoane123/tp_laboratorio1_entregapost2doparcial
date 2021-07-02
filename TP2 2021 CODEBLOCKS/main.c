49dcac1879bf1d8b1c0a9ac7db4dfaf7f84bb1d4
LeandroSeoane123 <52429721+LeandroSeoane123@users.noreply.github.com>
2021-07-02 02:59:12 -0300

    Add files via upload

---
+++ TP2 2021 CODEBLOCKS/main.c
@@ -0,0 +1,58 @@
+#define TE 1000
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayEmployees.h"
+#include "Seoane.h"
+
+int main()
+{
+
+   int opcion;
+   eEmpleado arrayDeEmpleados[TE];
+
+   hardcodearEmpleados(arrayDeEmpleados, TE);
+
+   do
+    {
+        getInt("***** ABM EMPLEADOS: MENU DE OPCIONES *****"
+               "\n1. Alta de empleado."
+               "\n2. Modificacion de empleado."
+               "\n3. Baja de empleado."
+               "\n4. Listado de empleados."
+               "\n5. Listado de empleados ordenados."
+               "\n6. Listar el total y el promedio de los salarios y cuantos empleados superan el salario promedio"
+               "\n7. Salir."
+               "\nIngrese la opcion que desea realizar: ", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+            cargarEmpleado(arrayDeEmpleados, TE);
+            break;
+        case 2:
+            modificarEmpleado(arrayDeEmpleados, TE);
+            break;
+        case 3:
+            borrarEmpleado(arrayDeEmpleados, TE);
+            break;
+        case 4:
+            mostrarEmpleados(arrayDeEmpleados, TE);
+            break;
+        case 5:
+            ordenarEmpleados(arrayDeEmpleados, TE);
+            break;
+        case 6:
+            mostrarTotalYPromedioSalariosYEmpleadosSuperiorPromedio(arrayDeEmpleados, TE);
+            break;
+        case 7:
+            puts("\nGracias por utilizar el programa!");
+            break;
+        }
+
+        validarMenu(opcion, 1, 7);
+        limpiarPantalla();
+
+    }while(opcion !=7);
+
+    return 0;
+}
